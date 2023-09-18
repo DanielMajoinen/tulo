575b7ae3a043a2063925c65c882a5aa7c91528c9
Daniel Majoinen <github@d.majoinen.com>
2023-09-18 13:00:16 +0000

    improvement: providers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import NextAuthProvider from '@/lib/auth/Provider'
-import TrpcProvider from '@/lib/trpc/Provider'
+import { Providers } from './providers'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,9 +14,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={inter.className}>
-        <NextAuthProvider>
-          <TrpcProvider>{children}</TrpcProvider>
-        </NextAuthProvider>
+        <Providers>{children}</Providers>
       </body>
     </html>
   )
