95fdd22f131bba3bcf85fd8efb68466622b32a03
Daniel Majoinen <github@d.majoinen.com>
2023-09-25 16:16:38 +1000

    chore: move axiom web vitals into _app.tsx

--- apps/www/src/components/layout.tsx
+++ apps/www/src/components/layout.tsx
@@ -1,13 +1,10 @@
 'use client'
 
-import { AxiomWebVitals } from 'next-axiom'
-
 import { ThemeToggle } from './theme-toggle'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <AxiomWebVitals />
       <div className="absolute bottom-1 right-1">
         <ThemeToggle />
       </div>
--- apps/www/src/pages/_app.tsx
+++ apps/www/src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { type Session } from 'next-auth'
 import { SessionProvider } from 'next-auth/react'
+import { AxiomWebVitals } from 'next-axiom'
 import { type AppType } from 'next/app'
 
 import { ThemeProvider } from '@/components/theme-provider'
@@ -10,6 +11,7 @@ import '@/styles/globals.css'
 const Tulo: AppType<{ session: Session | null }> = ({ Component, pageProps: { session, ...pageProps } }) => {
   return (
     <SessionProvider session={session}>
+      <AxiomWebVitals />
       <ThemeProvider>
         <Component {...pageProps} />
       </ThemeProvider>
