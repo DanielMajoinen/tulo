39c3c82fa751b57150c68fe4fa66f215507467ad
Daniel Majoinen <github@d.majoinen.com>
2023-09-25 23:15:45 +1000

    chore: remove welcome message

--- apps/www/src/pages/index.tsx
+++ apps/www/src/pages/index.tsx
@@ -1,33 +1,11 @@
 'use client'
 
 import DashboardLayout from '@/components/layouts/DashboardLayout'
-import { signIn, signOut, useSession } from 'next-auth/react'
 
 export default function Dashboard() {
   return (
     <DashboardLayout>
-      <div className="flex min-h-screen flex-col items-center justify-center">
-        <div className="flex items-baseline justify-between gap-x-1 py-8">
-          <h1 className="text-[5rem] font-light tracking-tight text-black selection:text-5xl dark:text-white">Welcome</h1>
-        </div>
-        <AuthShowcase />
-      </div>
+      <div className="flex min-h-screen flex-col items-center justify-center"></div>
     </DashboardLayout>
-  )
-}
-
-function AuthShowcase() {
-  const { data: sessionData } = useSession()
-
-  return (
-    <div className="flex flex-col items-center justify-center gap-4">
-      <p className="text-center text-2xl text-black dark:text-white">{sessionData && <span>Logged in as {sessionData.user?.name}</span>}</p>
-      <button
-        className="rounded-full bg-black/10 px-10 py-3 font-semibold text-black no-underline transition hover:bg-white/20 dark:bg-white/10 dark:text-white"
-        onClick={sessionData ? () => void signOut() : () => void signIn()}
-      >
-        {sessionData ? 'Sign out' : 'Sign in'}
-      </button>
-    </div>
   )
 }
