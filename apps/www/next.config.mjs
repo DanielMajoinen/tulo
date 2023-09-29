e643598bdde5293bf6f87652666c6bf627d813aa
Daniel Majoinen <github@d.majoinen.com>
2023-09-29 23:52:31 +1000

    feat: add initial PWA support

--- apps/www/next.config.mjs
+++ apps/www/next.config.mjs
@@ -4,20 +4,28 @@
  */
 await import('./src/env.mjs')
 import { withAxiom } from 'next-axiom'
+import NextPWA from 'next-pwa'
 
-/** @type {import("next").NextConfig} */
-const config = withAxiom({
-  reactStrictMode: true,
-
-  /**
-   * If you are using `appDir` then you must comment the below `i18n` config out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
-  i18n: {
-    locales: ['en'],
-    defaultLocale: 'en'
-  }
+const withPWA = NextPWA({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development'
 })
+
+/** @type {import("next").NextConfig & import("next-pwa").PWAConfig} */
+const config = withPWA(
+  withAxiom({
+    reactStrictMode: true,
+
+    /**
+     * If you are using `appDir` then you must comment the below `i18n` config out.
+     *
+     * @see https://github.com/vercel/next.js/issues/41980
+     */
+    i18n: {
+      locales: ['en'],
+      defaultLocale: 'en'
+    }
+  })
+)
 
 export default config
--- apps/www/src/components/layouts/RootLayout.tsx
+++ apps/www/src/components/layouts/RootLayout.tsx
@@ -1,9 +1,21 @@
 'use client'
 
+import Head from 'next/head'
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <div className="flex min-h-screen flex-col bg-white dark:bg-black">{children}</div>
+      {/* Add viewport settings for the PWA */}
+      <Head>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+        />
+      </Head>
+      {/* Page content */}
+      <main>
+        <div className="flex min-h-screen flex-col bg-white dark:bg-black">{children}</div>
+      </main>
     </>
   )
 }
