58bb47251df108f0360ef7337197b89f0cc07580
Daniel Majoinen <github@d.majoinen.com>
2023-10-08 16:25:58 +1100

    style: hide explore button on explore page

--- apps/www/src/components/menu/MenuHeader.tsx
+++ apps/www/src/components/menu/MenuHeader.tsx
@@ -13,19 +13,14 @@ export default function MenuHeader({ activePageName }: MenuHeaderProps = {}) {
   const isExplorePage = activePageName?.toLowerCase() === 'explore'
   return (
     <div className="flex flex-none items-center justify-center gap-2 xl:flex-col xl:gap-5">
-      <div>
-        <Icons.tulo className="h-10" color="#e66b6c" />
-      </div>
-      <div>
+      <Icons.tulo className="h-10" color="#e66b6c" />
+      {!isExplorePage && (
         <Link to="/">
-          <Button
-            className={`rounded-full border-2 border-[#e66b6c] hover:bg-[#e66b6c] ${isExplorePage ? 'bg-[#e66b6c]' : ''}`}
-            variant="outline"
-          >
+          <Button className={`rounded-full border-2 border-[#e66b6c] hover:bg-[#e66b6c]`} variant="outline">
             Explore
           </Button>
         </Link>
-      </div>
+      )}
     </div>
   )
 }
