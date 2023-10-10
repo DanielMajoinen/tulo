8e4a56bfd6c3c3a1d8c523069eb791a7fb993282
Daniel Majoinen <github@d.majoinen.com>
2023-10-10 13:38:02 +1100

    refactor: remove accounts store

--- apps/www/src/stores/accounts/hooks.ts
+++
@@ -1,6 +0,0 @@
-import { createHooks } from '@/stores/accounts/client/react'
-
-export { ClientDescriptor } from '@/stores/accounts/client/index'
-export { default as migrations } from '@/stores/accounts/migrations'
-
-export default createHooks()
