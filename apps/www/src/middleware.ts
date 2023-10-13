ad7a8e80561a6b3828529389427708226c667f77
Daniel Majoinen <github@d.majoinen.com>
2023-10-13 21:04:50 +1100

    chore: update middleware logic

--- apps/www/src/middleware.ts
+++ apps/www/src/middleware.ts
@@ -3,17 +3,14 @@ import { type NextRequestWithAuth, withAuth } from 'next-auth/middleware'
 
 export default withAuth(
   function middleware(req: NextRequestWithAuth) {
-    // Redirect unauthenticated users to login page
-    if (req.nextUrl.pathname !== '/login' && !req.nextauth.token) {
-      return NextResponse.redirect(new URL('/login', req.url))
+    // Always allow these requests
+    if (['/login', '/_vercel/'].some((path) => req.nextUrl.pathname.startsWith(path))) {
+      return NextResponse.next()
     }
 
-    // Allow requests to /api/* and /_vercel/* to pass through
-    const allowList = ['/api/', '/_vercel/']
-
-    if (req.nextUrl.pathname !== '/' && !allowList.some((path) => req.nextUrl.pathname.startsWith(path))) {
-      // Otherwise redirect to index page
-      return NextResponse.redirect(new URL('/', req.url))
+    // Redirect unauthenticated users to login page
+    if (!req.nextauth.token && req.nextUrl.pathname !== '/login') {
+      return NextResponse.redirect(new URL('/login', req.url))
     }
   },
   {
