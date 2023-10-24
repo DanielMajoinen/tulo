cbfae759f8e4260e43ba66ad97cc64c7e556b2b4
Daniel Majoinen <github@d.majoinen.com>
2023-10-24 21:10:52 +1100

    refactor: remove boards from trpc

--- apps/www/src/server/api/root.ts
+++ apps/www/src/server/api/root.ts
@@ -1,4 +1,3 @@
-import { boardsRouter } from '@/server/api/routers/boards'
 import { loginProvidersRouter } from '@/server/api/routers/loginProviders'
 import { createTRPCRouter } from '@/server/api/trpc'
 
@@ -8,7 +7,6 @@ import { createTRPCRouter } from '@/server/api/trpc'
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  boards: boardsRouter,
   loginProviders: loginProvidersRouter
 })
 
